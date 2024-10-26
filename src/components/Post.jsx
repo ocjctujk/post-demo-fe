5eddf7b9dd2dc0ed709e2ef0fa29be5453709780
Maharshi <007maharshigohel@gmail.com>
2024-10-25 21:54:44 -0400

    almost done with FE

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,38 @@
 import { useLocation } from "react-router-dom";
 import "./post.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Post = () => {
   const location = useLocation();
   const { postData } = location.state;
+  const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8088/posts/${postData._id}/comments`)
+      .then((data) => {
+        setComments(data.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [postData._id]);
+
+  const addCommentHandler = () => {
+    if (comment === "") {
+      return;
+    }
+    let commentObj = {
+      postId: postData._id,
+      content: comment.trim(),
+    };
+    axios.post(`http://localhost:8088/comments`, commentObj).then(() => {
+      setComments([...comments,commentObj]);
+      setComment("");
+    });
+  };
 
   // Ensure that you're rendering individual fields, not the object itself
   return (
@@ -12,11 +41,24 @@ const Post = () => {
       <p className="post-content">{postData.content}</p>
 
       <h3 className="comments-heading">Comments</h3>
+
+      <div className="add-comment">
+        <input
+          value={comment}
+          onChange={(e) => {
+            setComment(e.target.value);
+          }}
+        />
+        <button className="add-comment-btn" onClick={addCommentHandler}>
+          Add Comment
+        </button>
+      </div>
+
       <ul className="comments-list">
-        {postData.comments && postData.comments.length > 0 ? (
-          postData.comments.map((comment, index) => (
+        {comments && comments.length > 0 ? (
+          comments.toReversed().map((comment, index) => (
             <li key={index} className="comment-item">
-              {comment}
+              {comment.content}
             </li>
           ))
         ) : (
